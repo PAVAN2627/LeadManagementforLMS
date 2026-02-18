204dc2b3789e2ad603c7842f04ec9bf1074ddc59
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-02-18 14:10:35 +0530

    deleted

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { adminLogin } from "../controllers/authControllers.js";
-
-const router = express.Router();
-
-router.post("/admin/login", adminLogin);
-
-export default router;
